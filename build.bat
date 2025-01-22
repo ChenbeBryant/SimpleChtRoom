2bebdac668268c376f1ef2e6256e584a90c66116
ChenbeBryant <1808827231@qq.com>
2025-01-22 16:13:18 +0800

    update build script

--- build.bat
+++ build.bat
@@ -1,2 +1,5 @@
-g++ -o client ./client.cpp -lws2_32
-g++ -o server ./server.cpp -lws2_32
+;g++ -o client ./client.cpp -lws2_32
+;g++ -o server ./server.cpp -lws2_32
+
+g++ -o server.exe .\gameserver.h .\gameserver.cpp -lws2_32
+g++ -o client.exe .\gameserver.h .\gameclient.cpp -lws2_32
